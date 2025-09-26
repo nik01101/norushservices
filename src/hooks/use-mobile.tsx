b323745431c15ed30af4dfe51d5e04853980d767
Nikcolas Canessa Arguedas <nikco20013@gmail.com>
2025-09-26 19:36:43 +0000

    i also noticed that in safari the layout is not correct, it all stacks o

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -1,3 +1,6 @@
+
+"use client";
+
 import * as React from "react"
 
 const MOBILE_BREAKPOINT = 768
@@ -6,14 +9,23 @@ export function useIsMobile() {
   const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
 
   React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+    if (typeof window === 'undefined') {
+        return;
     }
+    
+    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
+    
+    const onChange = (e: MediaQueryListEvent) => {
+      setIsMobile(e.matches)
+    }
+
     mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+    
+    // Set the initial state
+    setIsMobile(mql.matches)
+
     return () => mql.removeEventListener("change", onChange)
   }, [])
 
-  return !!isMobile
+  return isMobile
 }
